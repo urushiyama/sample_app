c54b341f7a6bfe0aa3010fc2cbc6541d8ddcd484
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-14 20:53:56 +0900

    Add edit form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,9 +19,13 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
