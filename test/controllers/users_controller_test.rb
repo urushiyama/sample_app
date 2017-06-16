691d24d94f4ab3c557de355e73f72b4a0fd893ad
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-16 22:45:24 +0900

    Fix not to show nonactive users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,11 +5,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
+    @nonactive = users(:nonactive)
   end
 
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should redirect show when user is not activated" do
+    get user_path(@nonactive)
+    assert_redirected_to root_url
   end
 
   test "should get new" do
