8b878f786f8bcff97df9a3135c703352ecd76c88
urushiyama <aswif10flis1ntkb@gmail.com>
2017-06-30 16:31:50 +0900

    Add micropost model & view templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_many :microposts, dependent: :destroy
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
